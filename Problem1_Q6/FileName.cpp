d349148607c47debada3c9434c121edd3635db1d
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 22:21:10 +0200

    Add project files.

---
+++ Problem1_Q6/FileName.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//6.(Compute expressions) Write a program that displays the result of 
+
+	cout << (7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5);
+
+	return 0;
+}
